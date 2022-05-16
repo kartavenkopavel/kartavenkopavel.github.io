51819ea6438af460109c551e6cf5775014e2a90b
Pavel <63100397+kartavenkopavel@users.noreply.github.com>
2022-05-16 22:03:51 +0300

    website files

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+    var $page = $('html, body');
+    $('a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 400);
+        return false;
+    });
+
+    $('.menu-btn').on('click', function(){
+        $('.menu__list').toggleClass('active')
+    });
+});
