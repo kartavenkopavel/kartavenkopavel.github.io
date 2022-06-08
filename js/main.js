9a77c1292f7a25a47e836f4654167ef90e48ad9b
DESKTOP-MDKATNF\gregg <gregg190@gmail.com>
2022-06-08 23:50:50 +0300

    ver1.0

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 $(function(){
-    $('.slider__box').slick({
-        prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/arrow-left.svg" alt="arrow-left" ></img>',
-        nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/arrow-right.svg" alt="arrow-right" ></img>',
+    var $page = $('html, body');
+    $('a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 400);
+        return false;
     });
 
-    $('a[href*="#"]').click(function() {
-        $('html, body, footer').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 500);
+    $('.menu-btn').on('click', function(){
+        $('.menu__list').toggleClass('active')
     });
 });
