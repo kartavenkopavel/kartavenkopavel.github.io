161b9b4a2dccb34dc75353750fa54e31ccf81f28
DESKTOP-MDKATNF\gregg <gregg190@gmail.com>
2022-06-08 23:47:07 +0300

    upload website ver2.0

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 $(function(){
-    var $page = $('html, body');
-    $('a[href*="#"]').click(function() {
-        $page.animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 400);
-        return false;
+    $('.slider__box').slick({
+        prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/arrow-left.svg" alt="arrow-left" ></img>',
+        nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/arrow-right.svg" alt="arrow-right" ></img>',
     });
 
-    $('.menu-btn').on('click', function(){
-        $('.menu__list').toggleClass('active')
+    $('a[href*="#"]').click(function() {
+        $('html, body, footer').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
     });
 });
